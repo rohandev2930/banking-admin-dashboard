bc73eb080d1ae2f392a6833f18f0cd8eb2ff00af
Rohan2930 <rohansingh2930@gmail.com>
2026-05-18 19:41:35 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/rohan/bankingdashboard/BankingdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.bankingdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/bankingdashboard/BankingdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.bankingdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
